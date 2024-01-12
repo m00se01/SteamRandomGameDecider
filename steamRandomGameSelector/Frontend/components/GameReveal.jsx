73759884caa1a1202fd1e53bbbcd5a012a72a1ae
Brandon <crandon1327@gmail.com>
2024-01-12 00:10:41 -0600

    random shuffle reveal box

--- steamRandomGameSelector/Frontend/components/GameReveal.jsx
+++ steamRandomGameSelector/Frontend/components/GameReveal.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./GameReveal.css";
 
-export const GameReveal = () => {
+export const GameReveal = ({ numRolled, gameTitle }) => {
+  const [display, setDisplay] = useState("");
+
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  useEffect(() => {
+    const shuffle = () => {
+      let tempDisplay = gameTitle;
+      let iterations = 0;
+
+      console.log(gameTitle);
+
+      const interval = setInterval(() => {
+        setDisplay(
+          tempDisplay
+            .split("")
+            .map((letter, index) => {
+              if (index < iterations + 1) {
+                return tempDisplay[index];
+              }
+              return letters[Math.floor(Math.random() * 26)];
+            })
+            .join("")
+        );
+
+        if (iterations > tempDisplay.length) {
+          clearInterval(interval);
+        }
+
+        iterations += 1;
+      }, 50);
+    };
+
+    shuffle();
+  }, [numRolled, gameTitle]);
+
   return (
     <div>
       <div className="container">
-        <p>Output</p>
+        <p>{numRolled}</p>
+        {/* Reveal Animation, Spinner, Symbols to Letters,  */}
+        <p>{display}</p>
       </div>
+      {/* <button onClick={shuffle}>Test</button> */}
     </div>
   );
 };
--- steamRandomGameSelector/Frontend/src/App.jsx
+++ steamRandomGameSelector/Frontend/src/App.jsx
@@ -1,18 +1,28 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { GameReveal } from "../components/GameReveal";
 
 function App() {
   const [rollCount, setRollCount] = useState(3);
   const [rolledNum, setRolledNum] = useState(0);
-  const games = 379;
+  // const games = gamelist.length;
 
+  const gamelist = [
+    { appid: 1, name: "Dota 2", icon: "" },
+    { appid: 2, name: "CS 2", icon: "" },
+    { appid: 3, name: "Portal", icon: "" },
+    { appid: 4, name: "Portal 2", icon: "" },
+    { appid: 5, name: "Kenshi", icon: "" },
+    { appid: 6, name: "Persona 4", icon: "" },
+    { appid: 7, name: "Baldurs Gate 3", icon: "" },
+    { appid: 8, name: "Chrono Trigger", icon: "" },
+    { appid: 9, name: "Firewatch", icon: "" },
+  ];
+
+  const games = gamelist.length;
   const roll = () => {
     if (rollCount > 0) {
       setRolledNum(Math.floor(Math.random() * games));
-      console.log(rollCount);
-      console.log(`rollednum: ${rolledNum}`);
       setRollCount(rollCount - 1);
     } else {
       alert("no more rolls left");
@@ -27,13 +37,15 @@ function App() {
     <>
       <h1>Choose For Me</h1>
 
-      <label htmlFor="steamid">Enter Your Steam ID: </label>
-      <input type="text" />
+      <GameReveal numRolled={rolledNum} gameTitle={gamelist[rolledNum].name} />
+      <label htmlFor="steamid" className="steamid">
+        Enter Your Steam ID:{" "}
+      </label>
 
-      <p>{rolledNum}</p>
+      <input type="text" className="steamidInput" />
 
-      <p>Rolls Left: {rollCount} </p>
-      <button onClick={resetRoll}>reset</button>
+      <p className="rollCounter">Rolls Left: {rollCount} </p>
+      <button onClick={resetRoll}>Reset</button>
       <button onClick={roll}>Roll</button>
     </>
   );
